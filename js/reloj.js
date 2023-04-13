9ad06204a1d875887ca3d84377db4c8a65aa50fb
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-04-13 19:00:57 -0300

    agrego los eventos para mostrar hora actualizada

--- js/reloj.js
+++ js/reloj.js
@@ -1,5 +1,6 @@
+function obtenerFechaHora(){
 
-let fechaActual =  new Date();
+    let fechaActual =  new Date();
     //muestra la fecha del dispositivo donde se abre
     console.log(fechaActual);
     //DIA 
@@ -13,24 +14,41 @@ let fechaActual =  new Date();
     let hora = document.getElementById('hora');
 
     //jueves X 
-
+    
     const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
     const monthsOfYear = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
+    
     fecha.innerHTML = `${daysOfWeek[fechaActual.getDay()]}${fechaActual.getDate()} de ${monthsOfYear[fechaActual.getMonth()]}`
-
+    
     let horaActual = fechaActual.getHours();
     let minutosActuales = fechaActual.getMinutes();
     let segundosActuales = fechaActual.getSeconds();
     
-   // para agregar el cero que no muestra javascipt le debo concatenar el cero adelante como string
+    // para agregar el cero que no muestra javascipt le debo concatenar el cero adelante como string
    if(minutosActuales< 10){
     minutosActuales = '0' + minutosActuales;
-   }
-   if(segundosActuales< 10){
+}
+if(segundosActuales< 10){
     segundosActuales = '0' + segundosActuales;
-   }
-   if(horaActual< 10){
-   horaActual = '0' + horaActual;
-   }
-   hora.innerHTML= `${horaActual}: ${minutosActuales}:${segundosActuales}`
+}
+if(horaActual< 10){
+    horaActual = '0' + horaActual;
+}
+hora.innerHTML= `${horaActual}: ${minutosActuales}:${segundosActuales}`
+}
+function cambiarEstilo(color){
+    let section = document.getElementById(contenedorPrincipal);
+ section.className = 'fs-1' + color
+}
+
+let btnAzul =document.getElementById('btnAzul'),
+btnGris = document.getElementById('btnGris'),
+btnRojo = document.getElementById('btnRojo');
+
+btnAzul.addEventListener('click', ()=> cambiarEstilo('text-Azul'));
+btnGrisaddEventListener('click', ()=> cambiarEstilo('text-Azul'));
+btnRojoaddEventListener('click', ()=> cambiarEstilo('text-Azul'));
+
+//para obtenerlo cada 1000 milisegundos actualizado y funcione el reloj
+setInterval(obtenerFechaHora,1000);
+
